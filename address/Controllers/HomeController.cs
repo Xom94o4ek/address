3056e7530e5a4a8a50f01390d7f1b904be8e77e2
Xom94o4ek <irek199707@mail.ru>
2021-04-29 23:33:52 +0300

    Реализован поиск. "Кудрявый" код и дизайн. Нет API.

--- address/Controllers/HomeController.cs
+++ address/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using address.Models;
+using address.ModelsData;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using address.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace address.Controllers
 {
@@ -24,13 +26,54 @@ namespace address.Controllers
         //[Authorize] //
         public IActionResult Index()
         {
-            return View(db.Users.ToList());
+            //return View(db.Users.ToList());
             //return Content(User.Identity.Name);
+            return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public ActionResult Search(string RegionName, string LocalityName, string DistrictName, string StreetName, string HouseNum)
+        {
+
+            var result = from e in db.Houses
+                         join d in db.Streets on e.StreetId equals d.StreetId
+                         join p in db.Districts on d.DistrictId equals p.DistrictId
+                         join a in db.Localities on p.LocalityId equals a.LocalityId
+                         join b in db.Areas on a.AreaId equals b.AreaId
+                         join c in db.Regions on b.RegionId equals c.RegionId
+                         orderby e.Streets
+                         select new DataHouses { HouseId = e.HouseId, HouseNum = e.HouseNum, Index = e.Index, Floors = e.Floors, Entrances = e.Entrances, Flats = e.Flats, StreetId = d.DistrictId, StreetName = d.StreetName, DistrictId = p.DistrictId, DistrictName = p.DistrictName, LocalityId = a.LocalityId, LocalityName = a.LocalityName, AreaId = b.AreaId, AreaName = b.AreaName, RegionName = c.RegionName, RegionId = c.RegionId };
+
+            if (!String.IsNullOrEmpty(RegionName))
+            {
+                result = result.Where(s => s.RegionName.Contains(RegionName));
+            }
+
+            if (!String.IsNullOrEmpty(LocalityName))
+            {
+                result = result.Where(s => s.LocalityName.Contains(LocalityName));
+            }
+
+            if (!String.IsNullOrEmpty(DistrictName))
+            {
+                result = result.Where(s => s.DistrictName.Contains(DistrictName));
+            }
+
+            if (!String.IsNullOrEmpty(StreetName))
+            {
+                result = result.Where(s => s.StreetName.Contains(StreetName));
+            }
+
+            if (!String.IsNullOrEmpty(HouseNum))
+            {
+                result = result.Where(s => s.HouseNum.Contains(HouseNum));
+            }
+
+            return PartialView(result);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
